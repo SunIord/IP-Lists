adae383df34eb049ee83a63b5a13c75f0a092d90
João Pedro A. de Moraes <160938086+SunIord@users.noreply.github.com>
2024-08-29 14:55:15 -0300

    Create The Witches Are Back.py

---
+++ Lista 1/The Witches Are Back.py
@@ -0,0 +1,41 @@
+nome_bruxa = str(input())
+if nome_bruxa == "Winifred Sanderson" :
+  print ("O livro!! Sem ele, a mais poderosa das bruxas não conseguirá realizar seus feitiços.")
+elif nome_bruxa == "Sarah Sanderson" :
+  print ("Tapem os ouvidos, sua voz pode hipnotizar!!")
+elif nome_bruxa == "Mary Sanderson" :
+  print ("Seu ótimo olfato e péssimo senso de direção podem ser usados para confundí-la.")
+else :
+  print ("Só mais uma criança fantasiada, continuem atentos!")
+
+'''
+
+Passados 300 anos desde seus enforcamentos por práticas de bruxaria, as irmãs Winifred, Sarah e Mary Sanderson são despertadas por Max, um adolescente cético que não acredita em seres sobrenaturais, ao acender uma vela negra em uma casa abandonada para provar seu ponto a sua irmãzinha Dani e sua amiga Alisson.
+
+Agora, os três precisam bolar um plano para derrotar as bruxas e salvar todas as crianças da cidade. Para isso, eles perceberam que as irmãs são fortalecidas quando estão juntas e resolveram atacar cada uma separadamente, atingindo o seu ponto fraco. Sua missão é criar um programa que dada a irmã alvo, indique como Max, Dani e Alisson podem vencê-la.
+
+Input
+
+Você receberá uma única string indicando se a bruxa a ser enfrentada é Winifred Sanderson, Sarah Sanderson ou Mary Sanderson
+
+nome_bruxa
+
+Output
+
+Caso a bruxa seja Winifred, a saída será:
+
+"O livro!! Sem ele, a mais poderosa das bruxas não conseguirá realizar seus feitiços."
+
+Caso a bruxa seja Sarah:
+
+"Tapem os ouvidos, sua voz pode hipnotizar!!"
+
+Caso esteja enfrentando Mary:
+
+"Seu ótimo olfato e péssimo senso de direção podem ser usados para confundí-la."
+
+Se o nome recebido não corresponder a nenhum desses:
+
+"Só mais uma criança fantasiada, continuem atentos!"
+
+'''
